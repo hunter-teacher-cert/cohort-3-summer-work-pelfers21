b908de616f3731f5f72c9ef32a37558f79ae61c0
Halseyjhs <96216781+pelfers21@users.noreply.github.com>
2022-07-07 20:12:06 +0000

    Added and fixed Fence, Fib, Time

---
+++ programming/5/Driver.java
@@ -0,0 +1,50 @@
+ import java.io.*;
+import java.util.*;
+
+
+/**
+ * Time class by Team LucidThinkeren
+ 
+ * collaborators: Patti Elfers-Wygand, Adam Prado, Parmanand Mohanlall
+ */
+
+/**
+   INSTRUCTIONS:
+   This file contains the Driver starter code for our Time class.
+   
+   Add code in main to test all your methods
+   
+   Place this file in a folder named programming/5/Driver.java
+*/
+
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+	//declare a var of appropriate type to assign an instance of Time to
+	    Time t;
+      Time z;
+      Time a;
+	//assign var the address of a newly-apportioned Time object
+	t = new Time();
+      System.out.println(t.toString());
+      z = new Time(5, 7, 8);
+      System.out.println(z.toString());
+
+    z.set(1,2,3);
+     System.out.println(z.toString());  
+
+      t.set(5,5,5);
+      z.add(t);
+      System.out.println(z.toString());  
+
+      System.out.println(z.equals(t));
+      
+      a = new Time(6, 7, 8);
+      System.out.println(z.equals(a));
+        
+        
+    }//end main()
+
+}//end class 
---
+++ programming/5/Fib.java
@@ -0,0 +1,39 @@
+
+ 
+ /* collaborators: Patti Elfers-Wygand
+ */
+
+public class Fib
+{
+
+  
+  //your implementation of fib(n) here
+
+public static int fibo (int x){
+  if(x == 0 || x ==1){
+    return 1;
+  }
+  else{
+    return fibo(x-1) + fibo(x-2);
+  }
+}
+  public static void main( String[] args )
+  {
+    System.out.println(fibo(6));
+    
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      System.out.println( fib(0) ); // -> 0
+      System.out.println( fib(1) ); // -> 1
+      System.out.println( fib(2) ); // -> 1
+      System.out.println( fib(3) ); // -> 2
+      System.out.println( fib(4) ); // -> 3
+      System.out.println( fib(5) ); // -> 5
+      //now go big:
+      System.out.println( fib(10) ); // -> 55
+      System.out.println( fib(20) ); // -> 6765
+      System.out.println( fib(40) ); // -> 102334155
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+  }//end main()
+
+}//end class Fib
